1c579e8058e3f7c0db13b6ce53f833df1c855221
rusets <rusets3@gmail.com>
2025-09-18 18:23:40 -0500

    SNS: guard subscription + trimspace(alert_email)

--- infra/sns.tf
+++ infra/sns.tf
@@ -1,10 +1,20 @@
+# Email to send CloudWatch alarms (can be empty)
+variable "alert_email" {
+  type    = string
+  default = ""
+  description = "Email address to receive CloudWatch alarms. Leave empty to skip SNS subscription."
+}
 
 resource "aws_sns_topic" "alerts" {
   name = "${var.project_name}-${var.environment}-alerts"
+  tags = local.tags
 }
 
+# Create subscription only when email is provided
 resource "aws_sns_topic_subscription" "email" {
+  count     = var.alert_email != "" ? 1 : 0
   topic_arn = aws_sns_topic.alerts.arn
   protocol  = "email"
-  endpoint  = var.alert_email # после apply подтвердить письмо!
+  # trimspace убирает случайные пробелы/переводы строки
+  endpoint  = trimspace(var.alert_email)
 }
